2125baf6dfa14f41a3a51255ba6ebdccae3578bd
rizkysix <rizkysix666@gmail.com>
2024-09-13 14:48:42 +0800

    feat: new partner

--- dist/utils/allowed-domain.js
+++ dist/utils/allowed-domain.js
@@ -3,9 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AtLeastThisUrlAppear = exports.AllowedHostDomain = exports.AllowedUrlDomain = void 0;
 exports.AllowedUrlDomain = {
     "<hallobali>": "https://hallobali.id/",
+    "<daruratmilyader>": "https://daruratmilyader.com/",
+    "<ipaymu>": "https://ipaymu.com/",
 };
 exports.AllowedHostDomain = {
     "<hallobali>": "hallobali.id",
+    "<daruratmilyader>": "daruratmilyader.com",
+    "<ipaymu>": "ipaymu.com",
 };
 exports.AtLeastThisUrlAppear = "https://hallobali.id/";
 //# sourceMappingURL=allowed-domain.js.map
--- src/utils/allowed-domain.ts
+++ src/utils/allowed-domain.ts
@@ -2,10 +2,14 @@ import { AllowedHostDomainParameter } from "./paramater";
 
 export const AllowedUrlDomain: AllowedHostDomainParameter = {
   "<hallobali>": "https://hallobali.id/",
+  "<daruratmilyader>": "https://daruratmilyader.com/",
+  "<ipaymu>": "https://ipaymu.com/",
 };
 
 export const AllowedHostDomain: AllowedHostDomainParameter = {
   "<hallobali>": "hallobali.id",
+  "<daruratmilyader>": "daruratmilyader.com",
+  "<ipaymu>": "ipaymu.com",
 };
 
 export const AtLeastThisUrlAppear = "https://hallobali.id/"
