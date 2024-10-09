917fdde29b8999c7587a54c8c0888f323bad847c
rizkysix <rizkysix666@gmail.com>
2024-10-09 14:00:17 +0800

    feat: add laku jual

--- dist/utils/allowed-domain.js
+++ dist/utils/allowed-domain.js
@@ -5,21 +5,25 @@ exports.AllowedUrlDomain = {
     "<hallobali>": "https://hallobali.id/",
     "<daruratmilyader>": "https://daruratmilyader.com/",
     "<ipaymu>": "https://ipaymu.com/",
+    "<lakujual>": "https://lakujual.id/",
 };
 exports.AllowedHostDomain = {
     "<hallobali>": "hallobali.id",
     "<daruratmilyader>": "daruratmilyader.com",
     "<ipaymu>": "ipaymu.com",
+    "<lakujual>": "lakujual.id",
 };
 exports.AllowedUrlDomainComplete = {
     "<hallobali>": "https://hallobali.id/",
     "<daruratmilyader>": "https://daruratmilyader.com/",
     "<ipaymu>": "https://ipaymu.com/",
+    "<lakujual>": "https://lakujual.id/",
 };
 exports.AllowedHostDomainComplete = {
     "<hallobali>": "hallobali.id",
     "<daruratmilyader>": "daruratmilyader.com",
     "<ipaymu>": "ipaymu.com",
+    "<lakujual>": "lakujual.id",
 };
 exports.AtLeastThisUrlAppear = "https://hallobali.id/";
 //# sourceMappingURL=allowed-domain.js.map
--- src/utils/allowed-domain.ts
+++ src/utils/allowed-domain.ts
@@ -4,24 +4,28 @@ export const AllowedUrlDomain: AllowedHostDomainParameter = {
   "<hallobali>": "https://hallobali.id/",
   "<daruratmilyader>": "https://daruratmilyader.com/",
   "<ipaymu>": "https://ipaymu.com/",
+  "<lakujual>": "https://lakujual.id/",
 };
 
 export const AllowedHostDomain: AllowedHostDomainParameter = {
   "<hallobali>": "hallobali.id",
   "<daruratmilyader>": "daruratmilyader.com",
   "<ipaymu>": "ipaymu.com",
+  "<lakujual>": "lakujual.id",
 };
 
 export const AllowedUrlDomainComplete: AllowedHostDomainParameter = {
   "<hallobali>": "https://hallobali.id/",
   "<daruratmilyader>": "https://daruratmilyader.com/",
   "<ipaymu>": "https://ipaymu.com/",
+  "<lakujual>": "https://lakujual.id/",
 };
 
 export const AllowedHostDomainComplete: AllowedHostDomainParameter = {
   "<hallobali>": "hallobali.id",
   "<daruratmilyader>": "daruratmilyader.com",
   "<ipaymu>": "ipaymu.com",
+  "<lakujual>": "lakujual.id",
 };
 
 export const AtLeastThisUrlAppear = "https://hallobali.id/"
