15a573d79640739898f6b82e1cc39395321666de
rizkysix <rizkysix666@gmail.com>
2024-10-23 10:26:24 +0800

    feat: add sarung indonesia domain

--- dist/utils/allowed-domain.js
+++ dist/utils/allowed-domain.js
@@ -3,39 +3,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AtLeastThisUrlAppear = exports.AllowedHostDomainComplete = exports.AllowedUrlDomainComplete = exports.AllowedHostDomain = exports.AllowedUrlDomain = void 0;
 exports.AllowedUrlDomain = {
     "<hallobali>": "https://hallobali.id/",
-    "<daruratmilyader>": "https://daruratmilyader.com/",
+    // "<daruratmilyader>": "https://daruratmilyader.com/",
     "<ipaymu>": "https://ipaymu.com/",
     "<lakujual>": "https://lakujual.id/",
     "<lakustore>": "https://lakustore.id/",
     "<bebaskirim>": "https://bebaskirim.com/",
     "<resellr>": "https://resellr.id/",
+    "<sarungindonesia-com>": "https://sarungindonesia.com/",
+    "<sarungindonesia-co-id>": "https://sarungindonesia.co.id/",
 };
 exports.AllowedHostDomain = {
     "<hallobali>": "hallobali.id",
-    "<daruratmilyader>": "daruratmilyader.com",
+    // "<daruratmilyader>": "daruratmilyader.com",
     "<ipaymu>": "ipaymu.com",
     "<lakujual>": "lakujual.id",
     "<lakustore>": "lakustore.id",
     "<bebaskirim>": "bebaskirim.com",
     "<resellr>": "resellr.id",
+    "<sarungindonesia-com>": "sarungindonesia.com",
+    "<sarungindonesia-co-id>": "sarungindonesia.co.id",
 };
 exports.AllowedUrlDomainComplete = {
     "<hallobali>": "https://hallobali.id/",
-    "<daruratmilyader>": "https://daruratmilyader.com/",
+    //"<daruratmilyader>": "https://daruratmilyader.com/",
     "<ipaymu>": "https://ipaymu.com/",
     "<lakujual>": "https://lakujual.id/",
     "<lakustore>": "https://lakustore.id/",
     "<bebaskirim>": "https://bebaskirim.com/",
     "<resellr>": "https://resellr.id/",
+    "<sarungindonesia-com>": "https://sarungindonesia.com/",
+    "<sarungindonesia-co-id>": "https://sarungindonesia.co.id/",
 };
 exports.AllowedHostDomainComplete = {
     "<hallobali>": "hallobali.id",
-    "<daruratmilyader>": "daruratmilyader.com",
+    //"<daruratmilyader>": "daruratmilyader.com",
     "<ipaymu>": "ipaymu.com",
     "<lakujual>": "lakujual.id",
     "<lakustore>": "lakustore.id",
     "<bebaskirim>": "bebaskirim.com",
     "<resellr>": "resellr.id",
+    "<sarungindonesia-com>": "sarungindonesia.com",
+    "<sarungindonesia-co-id>": "sarungindonesia.co.id",
 };
 exports.AtLeastThisUrlAppear = "https://hallobali.id/";
 //# sourceMappingURL=allowed-domain.js.map
--- src/utils/allowed-domain.ts
+++ src/utils/allowed-domain.ts
@@ -2,42 +2,50 @@ import { AllowedHostDomainParameter } from "./paramater";
 
 export const AllowedUrlDomain: AllowedHostDomainParameter = {
   "<hallobali>": "https://hallobali.id/",
-  "<daruratmilyader>": "https://daruratmilyader.com/",
+ // "<daruratmilyader>": "https://daruratmilyader.com/",
   "<ipaymu>": "https://ipaymu.com/",
   "<lakujual>": "https://lakujual.id/",
   "<lakustore>": "https://lakustore.id/",
   "<bebaskirim>": "https://bebaskirim.com/",
   "<resellr>": "https://resellr.id/",
+  "<sarungindonesia-com>": "https://sarungindonesia.com/",
+  "<sarungindonesia-co-id>": "https://sarungindonesia.co.id/",
 };
 
 export const AllowedHostDomain: AllowedHostDomainParameter = {
   "<hallobali>": "hallobali.id",
-  "<daruratmilyader>": "daruratmilyader.com",
+ // "<daruratmilyader>": "daruratmilyader.com",
   "<ipaymu>": "ipaymu.com",
   "<lakujual>": "lakujual.id",
   "<lakustore>": "lakustore.id",
   "<bebaskirim>": "bebaskirim.com",
   "<resellr>": "resellr.id",
+  "<sarungindonesia-com>": "sarungindonesia.com",
+  "<sarungindonesia-co-id>": "sarungindonesia.co.id",
 };
 
 export const AllowedUrlDomainComplete: AllowedHostDomainParameter = {
   "<hallobali>": "https://hallobali.id/",
-  "<daruratmilyader>": "https://daruratmilyader.com/",
+  //"<daruratmilyader>": "https://daruratmilyader.com/",
   "<ipaymu>": "https://ipaymu.com/",
   "<lakujual>": "https://lakujual.id/",
   "<lakustore>": "https://lakustore.id/",
   "<bebaskirim>": "https://bebaskirim.com/",
   "<resellr>": "https://resellr.id/",
+  "<sarungindonesia-com>": "https://sarungindonesia.com/",
+  "<sarungindonesia-co-id>": "https://sarungindonesia.co.id/",
 };
 
 export const AllowedHostDomainComplete: AllowedHostDomainParameter = {
   "<hallobali>": "hallobali.id",
-  "<daruratmilyader>": "daruratmilyader.com",
+  //"<daruratmilyader>": "daruratmilyader.com",
   "<ipaymu>": "ipaymu.com",
   "<lakujual>": "lakujual.id",
   "<lakustore>": "lakustore.id",
   "<bebaskirim>": "bebaskirim.com",
   "<resellr>": "resellr.id",
+  "<sarungindonesia-com>": "sarungindonesia.com",
+  "<sarungindonesia-co-id>": "sarungindonesia.co.id",
 };
 
 export const AtLeastThisUrlAppear = "https://hallobali.id/"
